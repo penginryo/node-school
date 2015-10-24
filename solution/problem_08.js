da8b395aa3e2f9b4ad5832b46dc0f87c57987085
penginryo <penginryo@gmail.com>
2015-10-24 14:59:45 -0700

    remove comments

--- solution/problem_08.js
+++ solution/problem_08.js
@@ -2,9 +2,6 @@ var http = require('http');
 var bl = require('bl');
 var url = process.argv[2];
 
-// http(url).pipe(bl(function(err, data){
-//   console.log(data);
-// }));
 http.get(url, function(res){
   res.pipe(bl(function(err, data){
     if(err)
